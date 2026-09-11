bf181479f1ee911f16466891991154f12e14ef71
Santanu-2002 <your-github-email@example.com>
2026-09-11 20:16:16 +0530

    some changes are made

--- autonomous-test-orchestrator/src/models/UserHistory.js
+++ autonomous-test-orchestrator/src/models/UserHistory.js
@@ -1,5 +1,5 @@
 import mongoose, { mongo } from "mongoose";
-import userAuthMiddleware from "../middlewares/userAuthMiddleware";
+
 const userHistorySchema = mongoose.Schema(
     {
         userId:{
